7b1991d5fe46710dff50d08ef0f6d39ad803a862
stlndm <liamxujia@hotmail.com>
2018-10-18 11:23:37 +0800

    init news

---
+++ news/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by stlndm on 2018/10/18.
+//  Copyright © 2018 stlndm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
